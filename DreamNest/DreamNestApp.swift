9b1ab83373133ac4044719344f21f33b01c3f56e
FlamaHenkel <phuongnguyen.ibk@gmail.com>
2024-02-26 20:55:40 +0100

    Initial Commit

---
+++ DreamNest/DreamNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamNestApp.swift
+//  DreamNest
+//
+//  Created by Flama Henkel on 26.02.24.
+//
+
+import SwiftUI
+
+@main
+struct DreamNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
