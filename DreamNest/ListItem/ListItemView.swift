6cc76558ad8d67d94e065590cd48d16284ee4aa3
FlamaHenkel <phuongnguyen.ibk@gmail.com>
2024-02-27 22:38:58 +0100

    update ContentView

---
+++ DreamNest/ListItem/ListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListItemView.swift
+//  DreamNest
+//
+//  Created by Flama Henkel on 27.02.24.
+//
+
+import SwiftUI
+
+struct ListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListItemView()
+}
