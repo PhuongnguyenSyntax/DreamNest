2da2e92a299f59895844ce41ba17ace684c8d452
FlamaHenkel <phuongnguyen.ibk@gmail.com>
2024-02-27 15:56:52 +0100

    Login Screen

---
+++ DreamNest/Components/BottomView.swift
@@ -0,0 +1,18 @@
+//
+//  BottomView.swift
+//  DreamNest
+//
+//  Created by Flama Henkel on 27.02.24.
+//
+
+import SwiftUI
+
+struct BottomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomView()
+}
---
+++ DreamNest/Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  DreamNest
+//
+//  Created by Flama Henkel on 26.02.24.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
