e6a34d9f104dc6560ab98feb336ec4bcb2e230ca
FlamaHenkel <phuongnguyen.ibk@gmail.com>
2024-03-20 14:04:49 +0100

    update searchView

---
+++ DreamNest/Home/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  DreamNest
+//
+//  Created by Flama Henkel on 20.03.24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
